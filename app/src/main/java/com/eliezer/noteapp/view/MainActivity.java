3b08bd007287a3a21f43de6ba5a73a815d289f38
Eliezer Gensburger <eliezer.jct@gmail.com>
2020-11-01 18:04:13 +0200

    Note App

--- app/src/main/java/com/eliezer/noteapp/view/MainActivity.java
+++ app/src/main/java/com/eliezer/noteapp/view/MainActivity.java
@@ -1,4 +1,4 @@
-package com.eliezer.noteapp;
+package com.eliezer.noteapp.view;
 
 import android.os.Bundle;
 
@@ -10,8 +10,7 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
 
-import android.view.Menu;
-import android.view.MenuItem;
+import com.eliezer.noteapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,27 +29,5 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/eliezer/noteapp/viewmodel/NoteViewModel.java
+++ app/src/main/java/com/eliezer/noteapp/viewmodel/NoteViewModel.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class NoteViewModel extends AndroidViewModel {
 
     private NoteRepositoryWithExecutor repository;
-    private LiveData<List<Note>> allNotes;
+    private final  LiveData<List<Note>> allNotes;
 
     public NoteViewModel(@NonNull Application application) {
         super(application);
